012a0e710a18d3e28c1114b6d3eb6c2335ce3bfa
Alexander Vassilev <av@mega.co.nz>
2024-11-03 00:22:01 +0200

    Task fixes, support freertos modification to provide a memory release callback for stack and tcb

--- mySystem/task.hpp
+++ mySystem/task.hpp
@@ -2,15 +2,20 @@
 #define TASK_HPP
 
 #include <memory>
-#include "mutex.hpp"
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include "utils.hpp" // only for myassert()
+
+#define __TASK_MSG_OUT_OF_STACK_MEM "Out of memory allocating %ld bytes for %s stack for task %s"
 
 class Task
 {
 protected:
-    TaskHandle_t mHandle = nullptr;
+    volatile TaskHandle_t mHandle = nullptr;
     typedef void(Task::*MethodPtr)();
     static constexpr const char* kTag = "task";
     static constexpr const char* kMsgAlreadyHaveTask = "Already have task";
+#ifndef FREERTOS_HAVE_CUST_ALLOC_TASK
     struct PsramCtx {
         void* stack;
         StaticTask_t taskStruct;
@@ -36,41 +41,48 @@ protected:
         }
     };
     PsramCtx* mPsramCtx = nullptr;
+#endif
     union {
         TaskFunction_t mFunc;
         MethodPtr mMethod; // just a placeholder type
     };
     void* mArg;
-    bool destroyPsramCtx()
-    {
-        if (!mPsramCtx) {
-            return false;
-        }
-        PsramCtx::destroy(mPsramCtx);
-        mPsramCtx = nullptr;
-        return true;
-    }
     bool doCreate(const char* name, bool usePsram, uint32_t stackSize, BaseType_t cpuCore,
         UBaseType_t prio, TaskFunction_t func)
     {
         assert(!mHandle);
         if (usePsram) {
+#ifdef FREERTOS_TASK_HAVE_CUST_ALLOC
+            auto stack = (StackType_t*)heap_caps_malloc(stackSize, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+            if (!stack) {
+                ESP_LOGE(kTag, __TASK_MSG_OUT_OF_STACK_MEM, stackSize, "PSRAM", name);
+                return false;
+            }
+            auto tcb = (StaticTask_t*)heap_caps_malloc(sizeof(StaticTask_t), MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+            myassert(tcb);
+            mHandle = xTaskCreateCustAllocPinnedToCore(func, name, stackSize, this, prio, stack, tcb, cpuCore,
+                [](void* pStack, void* pTcb) {
+                    heap_caps_free(pStack);
+                    heap_caps_free(pTcb);
+                    printf("==========Freeing task stack and TCB\n");
+            });
+#else
             if (!mPsramCtx) {
                 mPsramCtx = PsramCtx::create(stackSize);
                 if (!mPsramCtx) {
-                    ESP_LOGE(kTag, "Out of memory allocating %lu PSRAM bytes stack for task %s", stackSize, name);
+                    ESP_LOGE(kTag, __TASK_MSG_OUT_OF_STACK_MEM, stackSize, "PSRAM", name);
                     return false;
                 }
             }
             mHandle = xTaskCreateStaticPinnedToCore(func, name, stackSize, this, prio,
                 (StackType_t*)mPsramCtx->stack, &mPsramCtx->taskStruct, cpuCore);
+#endif
         }
         else {
-            destroyPsramCtx();
-            auto ret = xTaskCreatePinnedToCore(func, name, stackSize, this, prio, &mHandle, cpuCore);
+            auto ret = xTaskCreatePinnedToCore(func, name, stackSize, this, prio, (TaskHandle_t*)&mHandle, cpuCore);
             if (ret != pdPASS) {
                 mHandle = nullptr;
-                ESP_LOGE(kTag, "Out of memory creating task %s with %lu bytes internal memory stack", name, stackSize);
+                ESP_LOGE(kTag, __TASK_MSG_OUT_OF_STACK_MEM, stackSize, "internal", name);
                 return false;
             }
         }
@@ -79,16 +91,38 @@ protected:
     }
     void cleanupBeforeExit()
     {
+        if (mPsramCtx) {
+            // TCB are cleanup in IDLE task, so give it some time
+            TimerHandle_t timer = xTimerCreate("cleanup", pdMS_TO_TICKS(4000), pdFALSE, mPsramCtx,
+                [](TimerHandle_t xTimer) {
+                    auto ctx = (PsramCtx*)pvTimerGetTimerID(xTimer);
+                    xTimerDelete(xTimer, portMAX_DELAY);
+                    PsramCtx::destroy(ctx);
+                    ESP_LOGW(kTag, "Freed task PSRAM stack and RCB");
+            });
+            mPsramCtx = nullptr;
+            xTimerStart(timer, portMAX_DELAY);
+        }
         mHandle = nullptr;
         vTaskDelete(nullptr);
     }
 public:
     TaskHandle_t handle() const { return mHandle; }
-    ~Task() {
-        if (mHandle) {
-            ESP_LOGE(kTag, "Destroying a running task");
+    void waitToEnd()
+    {
+        if (!mHandle) {
+            return;
         }
-        destroyPsramCtx();
+        int time = 0;
+        do {
+            vTaskDelay(1);
+            if ((++time % 100) == 0) {
+                ESP_LOGE(kTag, "Destructor: Timeout waiting for task to end");
+            }
+        } while(mHandle);
+    }
+    ~Task() {
+        waitToEnd();
     }
     template<class T>
     bool createTask(const char* name, bool usePsram, uint32_t stackSize, BaseType_t cpuCore,
@@ -120,12 +154,6 @@ public:
             ((TaskFunction_t)self.mFunc)(self.mArg);
             self.cleanupBeforeExit();
         });
-    }
-    bool freePsramStack() {
-        if (mHandle) {
-            return false;
-        }
-        return destroyPsramCtx();
     }
 };
 
